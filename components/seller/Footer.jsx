fabfa411471f5eda9cbd0f5ee8f4b7ebc87cb090
Mheyrie <omota4fidelity@gmail.com>
2025-04-21 19:20:47 +0100

    updated

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -1,6 +1,15 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 pap-10 py-14 border-b border-gray-500/30 text-gray-500">
+        <div className="w-4/5">
+          <Image src={assets.logo} alt="logo" className="w-28 md:w-32" />
+        </div>
+      </div>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
