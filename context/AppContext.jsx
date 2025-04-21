aaa2556ae38f3289df8dd085f176b21e15325e28
Mheyrie <omota4fidelity@gmail.com>
2025-04-21 14:06:03 +0100

    updating

---
+++ context/AppContext.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { productsDummyData, userDummyData } from "@/assets/assets";
+import App from "next/app";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AppContext = createContext();
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export const AppContextProvider = (props) => {
+  const router = useRouter();
+  const currency = process.env.NEXT_PUBLIC_CURRENCY;
+
+  const [products, setProducts] = useState([]);
+  const [userData, setUserData] = useState(false);
+  const [isSeller, setIsSeller] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const fetchProductData = async () => {
+    setProducts(productsDummyData);
+  };
+  const  value={
+        products,
+        userData,
+        setUserData,
+        isSeller,
+        setIsSeller,
+        cartItems,
+        setCartItems,
+        currency,
+      }
+    
+  return (
+    <AppContext.Provider value={value}>    
+      {props.children}
+    </AppContext.Provider>
+  );
+};
