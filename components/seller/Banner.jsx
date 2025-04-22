2590c31c07c862eb0d003af9974fdaab4651c00f
Mheyrie <omota4fidelity@gmail.com>
2025-04-22 18:26:52 +0100

    updated

--- components/seller/Banner.jsx
+++ components/seller/Banner.jsx
@@ -1,6 +1,6 @@
 function Banner() {
   return (
-    <div className="flex flex-col md:flex-row">Banner</div>
+    <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">Banner</div>
   )
 }
 export default Banner
