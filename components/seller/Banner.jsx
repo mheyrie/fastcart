764e78c13d684a97035cf4b2ef58a446a40907ba
Mheyrie <omota4fidelity@gmail.com>
2025-04-22 18:37:55 +0100

    updated

--- components/seller/Banner.jsx
+++ components/seller/Banner.jsx
@@ -8,6 +8,11 @@ function Banner() {
         src={assets.jbl_soundbox_image}
         alt="jbl_soundbox_image"
       />
+      <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
+        <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px] ">
+          Level Up Your Gaming Experience
+        </h2>
+      </div>
     </div>
   );
 }
