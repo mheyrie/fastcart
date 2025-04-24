740ad438f518887946dfa821848832046616bb0d
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:47:47 +0100

    updated

--- components/seller/ProductCard.jsx
+++ components/seller/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { assets } from "@/assets/assets";
 import { products } from "@/assets/productData";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -22,6 +23,36 @@ function ProductCard({ product }) {
         />
         <button className="absolute top-2 right-2bg-wite p-2 rounded-full shadow-md">
           <Image src={assets.heart_icon} alt="heart icon" className="w-3 h-3" />
+        </button>
+      </div>
+      <p className="md:text-base font-medium pt-2 w-full truncate">
+        {product.name}
+      </p>
+      <p className="w-full text-xs text-gray-500/70 max-sm:hidden truncate">
+        {product.description}
+      </p>
+      <div className="flex items-center gap-2">
+        <p className="text-xs">{4.5}</p>
+        <div className="flex items-center gap-0 5">
+          {Array.from({ lengtH: 5 }).map((_, index) => (
+            <Image
+              key={index}
+              src={
+                index < Math.floor(4) ? assets.star_icon : assets.star_dull_icon
+              }
+              alt="star icon"
+              className="w-3 h-3"
+            />
+          ))}
+        </div>
+      </div>
+      <div className="flex items-end justify-between w-full mt-1">
+        <p className="text-base font-medium">
+          {currency}
+          {product.offerPrice}
+        </p>
+        <button className=" max-sm:hidden px-4 py-1.5 text-gray-500 border border-gray-500/20 rounded-full text-xs hover:bg-slate-50 transition">
+          Buy now
         </button>
       </div>
     </div>
