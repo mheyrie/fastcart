591ab55fb81aaabda72ae51fa1d0e58dd9561248
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:36:50 +0100

    updated

--- components/seller/ProductCard.jsx
+++ components/seller/ProductCard.jsx
@@ -1,18 +1,30 @@
 import { products } from "@/assets/productData";
-import { useAppContext } from "@/context/AppContext"
+import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
 
-function ProductCard({product}) {
-  const {currency, router }=useAppContext()
+function ProductCard({ product }) {
+  const { currency, router } = useAppContext();
   return (
-    <div onClick={()=>{router.push('/product' + products._id); scrollTo(0, 0)}} className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer">
+    <div
+      onClick={() => {
+        router.push("/product" + products._id);
+        scrollTo(0, 0);
+      }}
+      className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
+    >
       <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
-      <Image src={product.image[0]} 
-      alt={product.name}
-      className="group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full" width={800} height={800} />
-    
+        <Image
+          src={product.image[0]}
+          alt={product.name}
+          className="group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full"
+          width={800}
+          height={800}
+        />
+        <button className="absolute top-2 right-2bg-wite p-2 rounded-full shadow-md">
+          <Image src={assets.heart_icon} alt="heart icon" className="w-3 h-3" />
+        </button>
       </div>
     </div>
-  )
+  );
 }
-export default ProductCard
+export default ProductCard;
