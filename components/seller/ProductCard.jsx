cb83f89b7720afbe7255370d81bada00011e91ce
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:35:43 +0100

    updated

--- components/seller/ProductCard.jsx
+++ components/seller/ProductCard.jsx
@@ -9,8 +9,8 @@ function ProductCard({product}) {
       <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
       <Image src={product.image[0]} 
       alt={product.name}
-      
-      />
+      className="group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full" width={800} height={800} />
+    
       </div>
     </div>
   )
