fa9e4b8d49bf94c6dfe97e3820cef27a6edbdbea
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:05:13 +0100

    updating header slider

--- components/seller/HeaderSlider.jsx
+++ components/seller/HeaderSlider.jsx
@@ -6,16 +6,16 @@ function HeaderSlider() {
   const sliderData = [
     {
       id: 1,
-      title: "Title 1",
+      title: "Experience Pure Sound - Your Perfect Headphones Awaits!",
       offer: "Limited Time Offer 30% Off",
-      buttonText1: "Shop Now",
-      buttonText2: "Learn More",
+      buttonText1: "Buy now",
+      buttonText2: "Find more",
       imgSrc: assets.header_headphone_image,
     },
     {
       id: 2,
-      title: "Title 2",
-      offer: "Hurry Up! Limited Edition left",
+      title: "Next-Level Gaming Starts Here - Discover PlayStation 5 Today!",
+      offer: "Hurry up only few lefts!",
       buttonText1: "Shop Now",
       buttonText2: "Explore Deals",
       imgSrc: assets.header_playstation_image,
@@ -23,16 +23,13 @@ function HeaderSlider() {
     {
       id: 3,
       title: "Power Meets Elegance - Apple MacBook Pro is Here for you!",
-      offer: "Exclusive Time Offer 40% Off",
+      offer: "Exclusive Deal 40% Off",
       buttonText1: "Order Now",
       buttonText2: "Learn More",
       imgSrc: assets.header_macbook_image,
     },
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-
-  const extendedSliderData = [...sliderData, sliderData[0]]; // Duplicate the first slide for seamless transition
 
   useEffect(() => {
     const interval = setInterval(() => {
