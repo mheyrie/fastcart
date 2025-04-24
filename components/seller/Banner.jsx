c8a38999c63b5dea1aa464012aef696faabf0466
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:16:45 +0100

    updated

--- components/seller/Banner.jsx
+++ components/seller/Banner.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 
@@ -14,15 +15,27 @@ function Banner() {
           Level Up Your Gaming Experience
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam magni impedit cumque saepe necessitatibus.
+          From immersive sound to precise controls—everything you need to win
         </p>
-        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
+        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600/70 rounded text-white">
           Buy now
-          <Image className="group-hover:translate-x-1 transition" src={assets.md_controller_image} alt="md_controller_image"/>
+          <Image
+            className="group-hover:translate-x-1 transition"
+            src={assets.arrow_icon_white}
+            alt="md_controller_image"
+          />
         </button>
       </div>
-      <Image className="hidden md:block max-w-80" src={assets.md_controller_image} alt="md_controller_image" />
-      <Image className="md:hidden" src={assets.sm_controller_image} alt="sm_controller_image" />
+      <Image
+        className="hidden md:block max-w-80"
+        src={assets.md_controller_image}
+        alt="md_controller_image"
+      />
+      <Image
+        className="md:hidden"
+        src={assets.sm_controller_image}
+        alt="sm_controller_image"
+      />
     </div>
   );
 }
