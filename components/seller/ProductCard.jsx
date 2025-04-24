59808da28baf11ee67ef01209ac20e782f595fb5
Mheyrie <omota4fidelity@gmail.com>
2025-04-24 03:34:06 +0100

    Product card implementation

--- components/seller/ProductCard.jsx
+++ components/seller/ProductCard.jsx
@@ -1,6 +1,18 @@
-function ProductCard() {
+import { products } from "@/assets/productData";
+import { useAppContext } from "@/context/AppContext"
+import Image from "next/image";
+
+function ProductCard({product}) {
+  const {currency, router }=useAppContext()
   return (
-    <div>ProductCard</div>
+    <div onClick={()=>{router.push('/product' + products._id); scrollTo(0, 0)}} className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer">
+      <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
+      <Image src={product.image[0]} 
+      alt={product.name}
+      
+      />
+      </div>
+    </div>
   )
 }
 export default ProductCard
