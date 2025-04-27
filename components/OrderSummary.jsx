0c6c67ce9f7073b8ea619268692a36ad577629de
Mheyrie <omota4fidelity@gmail.com>
2025-04-27 21:01:05 +0100

    updated

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,35 +1,38 @@
 import { addressDummyData } from "@/assets/assets";
-import { useAppContext } from "@/context/AppContext"
-import { useEffect, useState } from "react"
+import { useAppContext } from "@/context/AppContext";
+import { useEffect, useState } from "react";
 
 function OrderSummary() {
-
-  const {currency, router, getCartCount, getCartAmount} = useAppContext()
-  const [selectedAddress, setSelectedAddress]=useState(null);
+  const { currency, router, getCartCount, getCartAmount } = useAppContext();
+  const [selectedAddress, setSelectedAddress] = useState(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [userAddresses, setUserAddresses] = useState([])
+  const [userAddresses, setUserAddresses] = useState([]);
 
-  const fetchUserAddress = async ()=>{
-setUserAddresses(addressDummyData)
-  }
+  const fetchUserAddress = async () => {
+    setUserAddresses(addressDummyData);
+  };
 
-  const handleAddressSelect = (address)=>{
+  const handleAddressSelect = (address) => {
     setSelectedAddress(address);
-    setIsDropdownOpen(false)
-  }
+    setIsDropdownOpen(false);
+  };
 
-  const createOrder = async ()=>{
-    
-  }
+  const createOrder = async () => {};
 
-  useEffect(()=>{
-    fetchUserAddress()
-  }, [])
+  useEffect(() => {
+    fetchUserAddress();
+  }, []);
 
   return (
     <div className="w-full md:w-96 bg-gray-500/5 p-5">
       <h2 className="text-xl md:text-2xl">Order Summary</h2>
+     <hr className="border-gray-500/30 my-5"/>
+     <div className="space-y-6">
+      <div className="">
+       <label htmlFor="">Select Address</label>
+      </div>
+     </div>
     </div>
-  )
+  );
 }
-export default OrderSummary
+export default OrderSummary;
