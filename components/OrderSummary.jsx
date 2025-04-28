c488b7c7fdb862c4785a0795d5ab10dfaf97a794
Mheyrie <omota4fidelity@gmail.com>
2025-04-28 22:50:30 +0100

    updated

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -32,6 +32,55 @@ function OrderSummary() {
           <label className="text-base font-medium uppercase text-gray-700 focus:outline-none">
             Select Address
           </label>
+          <div className="relative inline-block w-full text-sm border">
+            <button
+              className="peer w-full text-left px-4 pr-2 py-2 bg-white text-gray-700 focus:outline-none"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <span>
+                {selectedAddress
+                  ? `${selectedAddress.fullName}, ${selectedAddress.area}, ${selectedAddress.city}, ${selectedAddress.state}`
+                  : "Select Address"}
+              </span>
+              <svg
+                className={`w-5 h-5 inline float-right transition-transform duration-200 ${
+                  isDropdownOpen ? "rotate-0" : "-rotate-90"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#6B7280"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+
+            {isDropdownOpen && (
+              <ul className="absolute w-full bg-white border shadow-md mt-1 z-10 py-1.5">
+                {userAddresses.map((address, index) => (
+                  <li
+                    key={index}
+                    className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer"
+                    onClick={() => handleAddressSelect(address)}
+                  >
+                    {address.fullName}, {address.area}, {address.city},{" "}
+                    {address.state}
+                  </li>
+                ))}
+                <li
+                  onClick={() => router.push("/add-address")}
+                  className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer text-center"
+                >
+                  + Add New Address
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
