525f44bbe6e84c66d5a447c6dbd0cdae9b565830
Mheyrie <omota4fidelity@gmail.com>
2025-05-10 20:23:28 +0000

    updated

--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -1,5 +1,18 @@
-export default function Navbar() {
+import React from 'react'
+import { assets } from '../../assets/assets'
+import Image from 'next/image'
+import { useAppContext } from '@/context/AppContext'
+
+const Navbar = () => {
+
+  const { router } = useAppContext()
+
   return (
-    <div>Navbar</div>
+    <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
+      <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer' src={assets.logo} alt="" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
   )
 }
+
+export default Navbar
