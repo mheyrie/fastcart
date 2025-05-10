7d021822bae86748d362fb470c7d4019efa46bbc
Mheyrie <omota4fidelity@gmail.com>
2025-05-10 20:54:07 +0000

    updated setting.json

---
+++ app/seller/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
+export default function AddProduct() {
+    
+  const [files, setFiles] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("Earphone");
+  const [price, setPrice] = useState("");
+  const [offerPrice, setOfferPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return <div>AddProduct</div>;
+}
