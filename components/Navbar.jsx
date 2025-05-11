592d47b2c5c5e37f822579ac39005a2b08cf4422
Mheyrie <omota4fidelity@gmail.com>
2025-05-11 22:06:02 +0000

    updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import ThemeToggle from "./theme/ThemeToggle";
 import { useClerk } from "@clerk/nextjs";
 
 function Navbar() {
-  const { isSeller, router } = useAppContext();
+  const { isSeller, router, user } = useAppContext();
   const { openSignIn } = useClerk();
 
   return (
@@ -47,13 +47,17 @@ function Navbar() {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button
-          onClick={openSignIn}
-          className="flex items-center gap-2 hover:text-gray-900 transition"
-        >
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {user ? (
+          <></>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { productsDummyData, userDummyData } from "@/assets/assets";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
@@ -13,6 +14,8 @@ export const useAppContext = () => {
 export const AppContextProvider = (props) => {
   const currency = process.env.NEXT_PUBLIC_CURRENCY;
   const router = useRouter();
+
+  const { user } = useUser();
 
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
@@ -79,6 +82,7 @@ export const AppContextProvider = (props) => {
   }, []);
 
   const value = {
+    user,
     currency,
     router,
     isSeller,
