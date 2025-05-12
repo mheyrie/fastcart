74d2e043a4853cda29402669d42efed560f1a5f2
Mheyrie <omota4fidelity@gmail.com>
2025-05-12 22:53:05 +0000

    updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -60,7 +60,7 @@ function Navbar() {
           </button>
         )}
       </ul>
-      //ANCHOR -
+      //TODO:
       <div className="flex items-center md:hidden gap-3">
         {isSeller && (
           <button
