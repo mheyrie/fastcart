00125c6f44ccd111651117e75e844375090e20d6
Mheyrie <omota4fidelity@gmail.com>
2025-05-17 21:23:53 +0000

    Add Inngest integration for user synchronization with Clerk
    - Updated package.json to include 'inngest' dependency.
    - Created inngest.js to define functions for syncing user creation and updates from Clerk to the database.
    - Implemented 'syncUserCreation' function to handle new user data and save it to the database.
    - Started implementation of 'syncUserUpdate' function for updating existing user data.

---
+++ config/inngest.js
@@ -0,0 +1,43 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "fastcart-next" });
+
+//Inngest function to save user data to the database
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.create(userData); //user is created here
+  }
+);
+
+//Inngets function to update user data in the database
+export const syncUserUpdate = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+  },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+  }
+);
