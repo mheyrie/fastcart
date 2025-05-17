6ba83dc914afddb1eaae845f1c9ab5b60eff0636
Mheyrie <omota4fidelity@gmail.com>
2025-05-17 16:05:49 +0000

    updated

--- config/db.js
+++ config/db.js
@@ -10,4 +10,10 @@ async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URL}`);
+  }
 }
