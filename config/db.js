beb6e7245af38c865df32108d49d22732427a2aa
Mheyrie <omota4fidelity@gmail.com>
2025-05-17 16:00:08 +0000

    updating mongoose config

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+}
