b1329339219f3ab0836395898723e34ec5b9f123
Mheyrie <omota4fidelity@gmail.com>
2025-05-19 23:00:01 +0000

    Implement product listing API, update product fetching in context, and enhance HomeProducts and ProductCard components

---
+++ app/api/product/list/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import Product from "@/models/Product";
+import connectDB from "@/config/db";
+
+export async function GET(request) {
+  try {
+    await connectDB();
+    const products = await Product.find({});
+    return NextResponse.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: error.message,
+    });
+  }
+}
