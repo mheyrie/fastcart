e5a38d96293528c0a691aec5071cbd423e36dab1
Mheyrie <omota4fidelity@gmail.com>
2025-05-20 00:44:10 +0000

    Implement order creation API with validation and amount calculation

---
+++ app/api/order/seller-orders/route.js
@@ -0,0 +1,38 @@
+import connectDB from "@/config/db";
+import Order from "@/models/Order";
+import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import Product from "@/models/Product";
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const { address, items } = await request.json();
+
+    if (!address || !items) {
+      return NextResponse.json({
+        success: false,
+        message: "Invalid data - Address and items are required",
+      });
+    }
+
+    //calculate amount using items
+    const amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return acc + product.offerPrice * item.quantity;
+    }, 0);
+
+    await connectDB();
+
+    return NextResponse.json({
+      success: true,
+      message: "Order placed successfully",
+      newOrder,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
